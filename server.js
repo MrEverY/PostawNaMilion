a9fa1ee8bd0742deeefdd20c30aaf9c35ddc687a
MrEverY <60273637+MrEverY@users.noreply.github.com>
2025-06-22 12:37:49 +0200

    dodajemy opcje hosta

--- server.js
+++ server.js
@@ -12,19 +12,31 @@ app.use(express.static(path.join(__dirname, "public")));
 const rooms = {};
 
 io.on("connection", (socket) => {
-  socket.on("joinRoom", (room) => {
+  socket.on("joinRoom", ({ room, isHost }) => {
     socket.join(room);
+    socket.room = room;
+    socket.isHost = isHost;
 
     if (!rooms[room]) {
       rooms[room] = {
         questionIndex: 0,
         cash: 1000000,
-        bets: [],
+        players: [],
+        hostId: null,
       };
     }
 
+    if (isHost) {
+      rooms[room].hostId = socket.id;
+    } else {
+      rooms[room].players.push(socket.id);
+    }
+  });
+
+  socket.on("startGame", (room) => {
     const q = questions[rooms[room].questionIndex];
-    if (q) io.to(room).emit("newQuestion", q);
+    io.to(room).emit("startGame");
+    io.to(room).emit("newQuestion", q);
   });
 
   socket.on("submitBet", ({ room, bet }) => {
@@ -34,33 +46,27 @@ io.on("connection", (socket) => {
     const question = questions[r.questionIndex];
     const poprawna = question.poprawna;
 
-    // Znajdź niepoprawne odpowiedzi
     const zleOdpowiedzi = ["A", "B", "C", "D"].filter(
-      (lit) => question.odpowiedzi[["A", "B", "C", "D"].indexOf(lit)] !== poprawna
+      (lit) =>
+        question.odpowiedzi[["A", "B", "C", "D"].indexOf(lit)] !== poprawna
     );
 
-    // Wybierz losową błędną odpowiedź do odrzucenia
     const odpada = zleOdpowiedzi[Math.floor(Math.random() * zleOdpowiedzi.length)];
-
-    // Odejmij kasę z tej odpowiedzi
     const utrata = bet[odpada] || 0;
     r.cash -= utrata;
     if (r.cash < 0) r.cash = 0;
 
-    // Poinformuj graczy, co odpadło i ile kasy zostało
     io.to(room).emit("odrzuconaOdpowiedz", {
       litera: odpada,
       poprawna,
       pozostalo: r.cash,
     });
 
-    // Po 4 sekundach prześlij kolejne pytanie lub zakończ grę
     setTimeout(() => {
       r.questionIndex++;
-
       if (r.questionIndex >= questions.length || r.cash <= 0) {
         io.to(room).emit("koniecGry", { wynik: r.cash });
-        delete rooms[room]; // wyczyść dane pokoju
+        delete rooms[room];
       } else {
         const nextQ = questions[r.questionIndex];
         io.to(room).emit("newQuestion", nextQ);
