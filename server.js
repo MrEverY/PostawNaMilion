19029c0764a1c46cb02216d0c1d39e827405f8dd
MrEverY <60273637+MrEverY@users.noreply.github.com>
2025-06-22 14:35:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
 const pytania = require("./pytania.json");
 
 app.use(express.static("public"));
@@ -17,8 +19,7 @@ io.on("connection", (socket) => {
       host: socket.id,
       players: [socket.id],
       currentQuestionIndex: 0,
-      remainingCash: 1000000,
-      approved: {},
+      remainingCash: 1000000
     };
     socket.emit("roomCreated", room);
     io.to(room).emit("updatePlayers", rooms[room].players);
@@ -28,7 +29,7 @@ io.on("connection", (socket) => {
     if (rooms[room]) {
       socket.join(room);
       rooms[room].players.push(socket.id);
-      io.to(socket.id).emit("roomJoined", room);
+      socket.emit("roomJoined", room);
       io.to(room).emit("updatePlayers", rooms[room].players);
     }
   });
@@ -38,30 +39,21 @@ io.on("connection", (socket) => {
   });
 
   socket.on("submitAnswer", ({ room, answers }) => {
-    rooms[room].lastAnswers = answers;
-    io.to(room).emit("stopTimer");
-    io.to(room).emit("showCheckButtons");
     const correct = pytania[rooms[room].currentQuestionIndex].poprawna;
-    io.to(rooms[room].host).emit("setCorrectAnswer", correct);
-  });
+    let total = 0;
 
-  socket.on("checkAnswer", ({ room, answer }) => {
-    const correct = pytania[rooms[room].currentQuestionIndex].poprawna;
-    const kwota = rooms[room].lastAnswers[answer] || 0;
-    if (answer !== correct) {
-      rooms[room].remainingCash -= kwota;
-      io.to(room).emit("answerChecked", {
-        answer,
-        correct: false,
-        remainingCash: rooms[room].remainingCash,
-      });
-    } else {
-      io.to(room).emit("answerChecked", {
-        answer,
-        correct: true,
-        remainingCash: rooms[room].remainingCash,
-      });
+    for (const [key, value] of Object.entries(answers)) {
+      if (key !== correct) {
+        rooms[room].remainingCash -= value;
+      } else {
+        total += value;
+      }
     }
+
+    io.to(room).emit("answerResult", {
+      correct,
+      remainingCash: rooms[room].remainingCash
+    });
   });
 
   socket.on("nextQuestion", (room) => {
@@ -69,30 +61,17 @@ io.on("connection", (socket) => {
     sendQuestion(room);
   });
 
-  socket.on("disconnect", () => {
-    for (let room in rooms) {
-      rooms[room].players = rooms[room].players.filter((id) => id !== socket.id);
-      if (rooms[room].players.length === 0) {
-        delete rooms[room];
-      } else {
-        io.to(room).emit("updatePlayers", rooms[room].players);
-      }
-    }
-  });
-});
-
-function sendQuestion(room) {
-  const index = rooms[room].currentQuestionIndex;
-  if (index < pytania.length) {
-    const q = pytania[index];
+  function sendQuestion(room) {
+    const qIndex = rooms[room].currentQuestionIndex;
+    const q = pytania[qIndex];
     io.to(room).emit("newQuestion", {
       pytanie: q.pytanie,
       odpowiedzi: q.odpowiedzi,
       cash: rooms[room].remainingCash
     });
-  } else {
-    io.to(room).emit("gameOver", rooms[room].remainingCash);
   }
-}
+});
 
-server.listen(3000, () => console.log("Serwer działa na porcie 3000"));
+server.listen(3000, () => {
+  console.log("Serwer działa na porcie 3000");
+});
